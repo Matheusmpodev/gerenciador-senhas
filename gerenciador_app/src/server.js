30376e3f62917892082bab7c00269f62c0f7e038
Matheusmpodev <Matheusmpo.dev@gmail.com>
2026-07-09 21:03:41 -0300

    Update server.js

--- gerenciador_app/src/server.js
+++ gerenciador_app/src/server.js
@@ -1,7 +1,14 @@
 import Fastify from "fastify"
+import fastifyStatic from "@fastify/static"
+import path from "node:path";
+import { fileURLToPath } from "node:url"
 const app = Fastify();
 const port = 3000;
 
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
+app.register (fastifyStatic, {
+    root: path.join(_dirname, "..", "public"),
+});
 app.get("/", async (request, reply) => {
     return "servidor online!";
 });
